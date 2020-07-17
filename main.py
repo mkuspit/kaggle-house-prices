9753943aef30ab4f95ad88eb4528eaec1b648742
Michal Kuspit <michal.kuspit@gmail.com>
2020-07-17 09:24:55 +0400

    Add main.py
    Added project structure

---
+++ main.py
@@ -0,0 +1,34 @@
+# Kaggle Competition
+# House Prices: Advanced Regression Techniques
+# https://www.kaggle.com/c/house-prices-advanced-regression-techniques
+
+
+#%% Import packages
+
+
+
+#%% Read data in
+
+
+
+#%% Analyze data
+
+
+
+#%% Engineer features
+
+
+
+#%% Build models
+
+
+
+#%% Finalize model
+
+
+
+#%% Create final predictions
+
+
+
+#%% Export solution
